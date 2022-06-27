1818679803588705df47044b8960fa1145cc3a00
Eugene Korshunov <emkorshunov@gmail.com>
2022-06-27 07:35:24 +0500

    exercise4 done

---
+++ work1/exercise4.js
@@ -0,0 +1,23 @@
+for (i = 1; i <=20; i++) {
+    if (i % 2 == 0) {
+        alert(i);
+    }
+}
+
+
+let i = 1;
+while (i<=20) {
+    if (i % 2 == 0) {
+        alert(i);
+    }
+    i = i + 1;
+}
+
+
+let i = 1;
+do {
+    if (i % 2 == 0) {
+        alert(i);
+    }
+    i = i + 1;
+} while (i <= 20);
