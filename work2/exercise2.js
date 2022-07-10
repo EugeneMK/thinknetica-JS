4b18e1857c2afc731241434821cdc2dd002e812c
Eugene Korshunov <emkorshunov@gmail.com>
2022-07-10 10:49:06 +0500

    fix exercise2

--- work2/exercise2.js
+++ work2/exercise2.js
@@ -1,8 +1,9 @@
 //2. Реализовать функцию для сравнения двух массивов, сравнение должно попарно сравнивать каждый элемент
 
-const compareArrays = (array1, array2) => {
-  const biggerArray = array1.length >= array2.length ? array1 : array2;
-  const smallerArray = array1.length < array2.length ? array1 : array2;
+  const compareArrays = (array1, array2) => {
+    if (array1.length !== array2.length) {
+      return false;
+    }
 
-  return biggerArray.map((element, index) => element === smallerArray[index]);
-};
+    return array1.every((item, i) => item === array2[i]);
+  };
