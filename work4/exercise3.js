bfe121a76c1357b13fa7aa50a24656358c0a6501
Eugene Korshunov <emkorshunov@gmail.com>
2022-08-02 00:20:21 +0500

    fix work4

--- work4/exercise3.js
+++ work4/exercise3.js
@@ -4,57 +4,123 @@
 // Человек → Сотрудник → Нынешний сотрудник/ бывший сотрудник
 
 // Есть следующий объект, в котором свойства лежат кучей:
-const john = {
-  name: "John",
-  lastName: "Smith",
-  position: "Senior engineer",
-  startDate: "10.10.1990",
-  endDate: "10.10.2000",
-  baseSalary: "10000",
-  salaryCurrency: "$",
-  location: "Russia",
-  department: "IT",
-  phoneNumber: "+1234567890",
-  eat: function () {},
-  sleep: function () {},
-  callFriend: function () {},
-  writeReport: function () {},
-  organizeMeeting: function () {},
-  retire: function () {},
-  startVacation: function () {},
+// const john = {
+//   name: "John",
+//   lastName: "Smith",
+//   position: "Senior engineer",
+//   startDate: "10.10.1990",
+//   endDate: "10.10.2000",
+//   baseSalary: "10000",
+//   salaryCurrency: "$",
+//   location: "Russia",
+//   department: "IT",
+//   phoneNumber: "+1234567890",
+//   eat: function () {},
+//   sleep: function () {},
+//   callFriend: function () {},
+//   writeReport: function () {},
+//   organizeMeeting: function () {},
+//   retire: function () {},
+//   startVacation: function () {},
+// };
+
+const Person = function (name, lastName, location, phoneNumber) {
+  this.name = name;
+  this.lastName = lastName;
+  this.location = location;
+  this.phoneNumber = phoneNumber;
+
+  this.eat = function () {
+    console.log(this.name + " is eating");
+  };
+  this.sleep = function () {
+    console.log(this.name + " is sleeping");
+  };
+  this.callFriend = function () {
+    console.log(this.name + " is calling friends");
+  };
 };
 
-const human = {
-  name: "John",
-  lastName: "Smith",
-  location: "Russia",
-  phoneNumber: "+1234567890",
-  eat: function () {},
-  sleep: function () {},
-  callFriend: function () {},
+const Employee = function (
+  name,
+  lastName,
+  location,
+  phoneNumber,
+  position,
+  baseSalary,
+  salaryCurrency,
+  department
+) {
+  this.name = name;
+  this.lastName = lastName;
+  this.location = location;
+  this.phoneNumber = phoneNumber;
+  this.position = position;
+  this.baseSalary = baseSalary;
+  this.salaryCurrency = salaryCurrency;
+  this.department = department;
+
+  this.writeReport = function () {
+    console.log(this.name + "is writing report");
+  };
+  this.organizeMeeting = function () {
+    console.log(this.name + "is organizing meeting");
+  };
 };
 
-const employee = {
-  position: "Senior engineer",
-  baseSalary: "10000",
-  salaryCurrency: "$",
-  department: "IT",
-  writeReport: function () {},
-  organizeMeeting: function () {},
+const CurrentEmployee = function (
+  name,
+  lastName,
+  location,
+  phoneNumber,
+  position,
+  baseSalary,
+  salaryCurrency,
+  department,
+  startDate
+) {
+  this.name = name;
+  this.lastName = lastName;
+  this.location = location;
+  this.phoneNumber = phoneNumber;
+  this.position = position;
+  this.baseSalary = baseSalary;
+  this.salaryCurrency = salaryCurrency;
+  this.department = department;
+  this.startDate = startDate;
+
+  this.startVacation = function () {
+    console.log(this.name + "is going on vacation");
+  };
+  this.retire = function () {
+    console.log(this.name + "retired");
+  };
 };
 
-Object.setPrototypeOf(employee, human);
-
-const currentEmployee = {
-  startDate: "10.10.1990",
-  startVacation: function () {},
-  retire: function () {},
+const FormerEmployee = function (
+  name,
+  lastName,
+  location,
+  phoneNumber,
+  position,
+  baseSalary,
+  salaryCurrency,
+  department,
+  startDate,
+  endDate
+) {
+  this.name = name;
+  this.lastName = lastName;
+  this.location = location;
+  this.phoneNumber = phoneNumber;
+  this.position = position;
+  this.baseSalary = baseSalary;
+  this.salaryCurrency = salaryCurrency;
+  this.department = department;
+  this.startDate = startDate;
+  this.endDate = endDate;
 };
 
-Object.setPrototypeOf(currentEmployee, employee);
-
-const formerEmployee = {
-  endDate: "10.10.2000",
-};
-
-Object.setPrototypeOf(formerEmployee, employee);
+Employee.prototype = new Person();
+CurrentEmployee.prototype = new Employee();
+FormerEmployee.prototype = new Employee();
